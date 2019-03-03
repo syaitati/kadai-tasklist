05bc61515cbd2e2e8c063a8c0c45491029d486d4
satoshi.mizobuchi <syaitati@gmail.com>
2019-03-03 21:28:32 +0900

    Add getAllTasks

--- src/models/Tasks.java
+++ src/models/Tasks.java
@@ -7,26 +7,32 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "messages")
-public class Message {
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllTasks",
+            query = "SELECT m FROM Tasks AS m ORDER BY m.id DESC"
+            )
+})
+@Table(name = "tasks")
+public class Tasks {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "created_at",nullable = false)
+    @Column(name = "created_at", nullable = false)
     private Timestamp created_at;
 
-    @Column(name = "updated_at",nullable = false)
+    @Column(name = "updated_at", nullable = false)
     private Timestamp updated_at;
 
     @Column(name = "content", length = 255, nullable = false)
     private String content;
-
-
 
     public Integer getId() {
         return id;
@@ -35,6 +41,7 @@ public class Message {
     public void setId(Integer id) {
         this.id = id;
     }
+
     public Timestamp getCreated_at() {
         return created_at;
     }
@@ -42,6 +49,7 @@ public class Message {
     public void setCreated_at(Timestamp created_at) {
         this.created_at = created_at;
     }
+
     public Timestamp getUpdated_at() {
         return updated_at;
     }
